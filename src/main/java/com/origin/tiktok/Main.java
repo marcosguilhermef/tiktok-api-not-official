14989735bba3f6aeb02ee61c46d7b46d5d3215bc
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-02-27 03:31:01 -0300

    first

--- src/main/java/com/origin/tiktok/Main.java
+++ src/main/java/com/origin/tiktok/Main.java
@@ -2,6 +2,7 @@ package com.origin.tiktok;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Teste 1");
     }
 }
