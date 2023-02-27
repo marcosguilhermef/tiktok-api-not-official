446f2fb642ad7a45bcf811a418055d68ab547bc2
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-02-27 03:48:36 -0300

    ajustando git

--- src/main/java/com/origin/tiktok/Main.java
+++ src/main/java/com/origin/tiktok/Main.java
@@ -2,6 +2,6 @@ package com.origin.tiktok;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste 1");
+        System.out.println("Teste 2");
     }
 }
